c893e1781a9404128eab9b65d08c1b8b49698f72
Andrei Maxim <andrei@andreimaxim.ro>
2018-02-08 15:38:12 +0200

    Triggering badges.

--- test/active_metrics_test.rb
+++ test/active_metrics_test.rb
@@ -1,11 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class ActiveMetricsTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ActiveMetrics::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
